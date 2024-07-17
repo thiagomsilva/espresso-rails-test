97f6a7c6a606669d409899914e27ad1610e4d1be
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2024-07-17 19:17:08 -0300

    chore: Add User no projeto

---
+++ db/migrate/20240717221328_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.boolean :admin
+      t.references :company, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
